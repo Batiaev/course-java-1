5a200a4bfac62d7444c49f3ce7941cb7c444ded2
Anton Batiaev <a.batyaev@gmail.com>
2017-12-25 22:06:39 +0300

    add lesson6 homework and lesson7

---
+++ src/com/batiaev/java1/lesson7/Plate.java
@@ -0,0 +1,23 @@
+package com.batiaev.java1.lesson7;
+
+/**
+ * Plate
+ *
+ * @author anton
+ * @since 25/12/17
+ */
+public class Plate {
+    private int food = 15;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void setFood(int food) {
+        this.food = food;
+    }
+}
